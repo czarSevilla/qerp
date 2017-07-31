6df63ac240f82b3bf34255eb6bfad1e8ff1c092d
juan <juan.bautista@qacg.com>
2017-07-31 11:40:11 -0500

    Actualización de BD para SEPOMEX

--- qerp-database/src/main/resources/qerp20170731_2.sql
+++ qerp-database/src/main/resources/qerp20170731_2.sql
@@ -17,7 +17,7 @@ USE `qerp`;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-
+--
 --
 -- Table structure for table `address_mx`
 --
