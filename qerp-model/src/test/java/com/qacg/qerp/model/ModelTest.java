80866c630f01c6841e5a0750dd1c348725d30f3f
Cesar Sevilla <cesar.sevilla@qacg.com>
2017-07-31 19:09:22 -0500

    se incluyen pruebas unitarias del modelo

--- qerp-model/src/test/java/com/qacg/qerp/model/ModelTest.java
+++ qerp-model/src/test/java/com/qacg/qerp/model/ModelTest.java
@@ -3,11 +3,18 @@ package com.qacg.qerp.model;
 import org.junit.Test;
 
 import com.qacg.qerp.model.DtoTester.BoolGetter;
+import com.qacg.qerp.model.dto.CompanyDto;
 import com.qacg.qerp.model.dto.CurrencyDto;
 import com.qacg.qerp.model.dto.DocumentTypeDto;
 import com.qacg.qerp.model.dto.EmployeeDto;
 import com.qacg.qerp.model.dto.JobTitleDto;
+import com.qacg.qerp.model.dto.LanguageDto;
 import com.qacg.qerp.model.dto.PhysicalResourceTypeDto;
+import com.qacg.qerp.model.dto.ResourceFeatureDto;
+import com.qacg.qerp.model.dto.SecModuleDto;
+import com.qacg.qerp.model.dto.SecPermissionDto;
+import com.qacg.qerp.model.dto.SecRoleDto;
+import com.qacg.qerp.model.dto.SecUserDto;
 
 public class ModelTest {
 	@Test
@@ -44,4 +51,55 @@ public class ModelTest {
 		
 		tester.verify();
 	}
+	
+	@Test
+     public void companyTest() {
+          DtoTester<CompanyDto> tester = new DtoTester<>(CompanyDto.class, BoolGetter.IS);
+          
+          tester.verify();
+     }
+	
+	@Test
+     public void resourceFeatureTest() {
+          DtoTester<ResourceFeatureDto> tester = new DtoTester<>(ResourceFeatureDto.class, BoolGetter.IS);
+          
+          tester.verify();
+     }
+	
+	@Test
+     public void secUserTest() {
+          DtoTester<SecUserDto> tester = new DtoTester<>(SecUserDto.class, BoolGetter.IS);
+          
+          tester.verify();
+     }
+	
+	@Test
+     public void languageTest() {
+          DtoTester<LanguageDto> tester = new DtoTester<>(LanguageDto.class, BoolGetter.IS);
+          
+          tester.verify();
+     }
+	
+	@Test
+     public void secModuleTest() {
+          DtoTester<SecModuleDto> tester = new DtoTester<>(SecModuleDto.class, BoolGetter.IS);
+          
+          tester.verify();
+     }
+	
+	@Test
+     public void secPermissionTest() {
+          DtoTester<SecPermissionDto> tester = new DtoTester<>(SecPermissionDto.class, BoolGetter.IS);
+          
+          tester.verify();
+     }
+	
+	@Test
+     public void secRoleTest() {
+          DtoTester<SecRoleDto> tester = new DtoTester<>(SecRoleDto.class, BoolGetter.IS);
+          
+          tester.verify();
+     }
+	
+	
 }
